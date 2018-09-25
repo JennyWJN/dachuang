a2fbdab43572afdeddde56b92f96ab0fdf510653
Jianing Wang <37525646+JennyWJN@users.noreply.github.com>
2018-09-25 20:54:14 +0800

    Add files via upload

---
+++ Gateway2.0.ino
@@ -0,0 +1,174 @@
+/*
+  LoRa Multiple Communication
+
+  This example provide a simple way to achieve one to multiple devices
+  communication.
+
+  Each devices send datas in broadcast method. Make sure each devices
+  working in the same BAND, then set the localAddress and destination
+  as you want.
+  
+  Sends a message every half second, and polls轮询 continually
+  for new incoming messages. Implements a one-byte addressing scheme单字节寻址,
+  with 0xFD as the broadcast address. You can set this address as you
+  want.
+
+  Note: while sending, LoRa radio is not listening for incoming messages.
+  
+  by Aaron.Lee from HelTec AutoMation, ChengDu, China
+  成都惠利特自动化科技有限公司
+  www.heltec.cn
+  
+  this project also realess in GitHub:
+  https://github.com/Heltec-Aaron-Lee/WiFi_Kit_series
+*/
+
+#include <SPI.h>
+#include <LoRa.h>
+#include <Wire.h>  
+#include "SSD1306.h" 
+#include "images.h"
+
+
+
+// Pin definetion of WIFI LoRa 32
+// HelTec AutoMation 2017 support@heltec.cn 
+#define SCK     5    // GPIO5  -- SX1278's SCK
+#define MISO    19   // GPIO19 -- SX1278's MISO
+#define MOSI    27   // GPIO27 -- SX1278's MOSI
+#define SS      18   // GPIO18 -- SX1278's CS
+#define RST     14   // GPIO14 -- SX1278's RESET
+#define DI00     26   // GPIO26 -- SX1278's IRQ(Interrupt Request) DI0 DI00???
+#define SDA    4
+#define SCL   15
+#define SD1306RST   16 //RST must be set by software
+
+#define BAND    433E6  //you can set band here directly,e.g. 868E6,915E6
+#define PABOOST true
+
+SSD1306  display(0x3c, SDA, SCL, SD1306RST);
+
+String outgoing;              // outgoing message
+String incoming;              // incoming message
+
+byte localAddress = 0xBB;     // address of this device
+byte destination = 0x00;      // destination to send to
+
+byte msgCount = 0;            // count of outgoing messages
+long lastSendTime = 0;        // last send time
+int interval = 20000;          // interval between sends
+
+void logo(){
+  display.clear();
+  display.drawXbm(0,5,logo_width,logo_height,(uint8_t*)logo_bits);
+  display.display();
+}
+
+void setup() {
+  Serial.begin(115200);                   // initialize serial
+  while (!Serial);
+
+  Serial.println("LoRa Duplex");
+  
+  pinMode(16,OUTPUT);
+  pinMode(25,OUTPUT);
+  
+  digitalWrite(16, LOW);    // set GPIO16 low to reset OLED
+  delay(50); 
+  digitalWrite(16, HIGH); // while OLED is running, must set GPIO16 in high
+  
+  display.init();
+  display.flipScreenVertically();  
+  display.setFont(ArialMT_Plain_10);
+  logo();
+  delay(1500);
+  display.clear();
+  
+  SPI.begin(SCK,MISO,MOSI,SS);
+  LoRa.setPins(SS,RST,DI00);
+  
+  if (!LoRa.begin(BAND,PABOOST)) {
+    display.drawString(0, 0, "Starting LoRa failed!");
+    display.display();
+    while (1);
+  }
+  display.drawString(0, 0, "LoRa Initial success!");
+  display.display();
+  delay(1000);
+}
+void loop()
+{
+  if (millis() - lastSendTime > interval)
+  {
+    String message = "Hello,I'm coming!";   // send a message
+    sendMessage(message);
+    Serial.println("Sending " + message + "to"+ destination);
+    lastSendTime = millis();            // timestamp the message
+    interval = random(20000) + 10000;    // 2-3 seconds
+    if(destination < 1)
+      destination++;
+    else
+      destination = 0;
+  }
+
+  // parse for a packet, and call onReceive with the result:
+  onReceive(LoRa.parsePacket());
+}
+
+void sendMessage(String outgoing)
+{
+  LoRa.beginPacket();                   // start packet
+  LoRa.write(destination);              // add destination address
+  LoRa.write(localAddress);             // add sender address
+  LoRa.write(msgCount);                 // add message ID
+  LoRa.write(outgoing.length());        // add payload length
+  LoRa.print(outgoing);                 // add payload
+  LoRa.endPacket();                     // finish packet and send it
+  msgCount++;                           // increment message ID
+}
+
+void onReceive(int packetSize)
+{
+  if (packetSize == 0) return;          // if there's no packet, return
+
+  // read packet header bytes:
+  int recipient = LoRa.read();          // recipient address
+  byte sender = LoRa.read();            // sender address
+  byte incomingMsgId = LoRa.read();     // incoming msg ID
+  byte incomingLength = LoRa.read();    // incoming msg length
+
+  String incoming = "";
+
+  while (LoRa.available())
+  {
+    incoming += (char)LoRa.read();
+  }
+
+  if (incomingLength != incoming.length())
+  {   // check length for error
+    Serial.println("error: message length does not match length");
+    return;                             // skip rest of function
+  }
+
+  // if the recipient isn't this device or broadcast,
+  if (recipient != localAddress && recipient != 0xFF) {
+    Serial.println("This message is not for me.");
+    return;                             // skip rest of function
+  }
+
+  // if message is for this device, or broadcast, print details:
+  Serial.println("Received from: 0x" + String(sender, HEX));
+  Serial.println("Sent to: 0x" + String(recipient, HEX));
+  Serial.println("Message ID: " + String(incomingMsgId));
+  Serial.println("Message length: " + String(incomingLength));
+  Serial.println("Message: " + incoming);
+  Serial.println("RSSI: " + String(LoRa.packetRssi()));
+  Serial.println("Snr: " + String(LoRa.packetSnr()));
+  Serial.println();
+  display.clear();
+  display.setTextAlignment(TEXT_ALIGN_LEFT);
+  display.setFont(ArialMT_Plain_10);
+  display.drawStringMaxWidth(0 , 26 , 128, incoming);
+  display.display();
+}
+
